840c55b5ec7f975277b4941452c5d3eda3fc7b06
RMInya <94555609+RMInya@users.noreply.github.com>
2022-08-26 00:03:50 +0300

    Add files via upload

--- App.java
+++ App.java
@@ -1,51 +1,30 @@
 import java.util.*;
 
 public class App {
-    int a = 2, b = 1, c, i, num;
-
+    int a = 2, b = 1, c, i, len;
+    int[] LucasNum = new int[100];
+    int[] TriangleNum = new int[100];
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
 
-    public static void main(String[] args) {
-        App obj = new App();
-        // змінні і масиви
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the length of lucas series:");
-        int len = in.nextInt();
-        int[] LucasNum = new int[len];
-        int[] TriangleNum = new int[len];
-        int j = 0;
-        int count = 2;
-        // масив чисел Люка
-        for (obj.i = 1; obj.i <= len; obj.i++) {
-            obj.c = obj.a + obj.b;
-            LucasNum[j] = obj.c;
-            obj.a = obj.b;
-            obj.b = obj.c;
-            j++;
-        }
-        // вивід чисел Люка
-        System.out.print(System.lineSeparator());
-        System.out.print(ANSI_GREEN + "Lucas Numbers: " + ANSI_RESET);
-        for (int k = 0; k < len; k++) {
-            System.out.print(LucasNum[k] + " ");
-        }
-        // масив трикутних чисел
-        for (int k = 0; k < len; k++) {
-            TriangleNum[k] = ((count * (count + 1)) / 2);
-            count++;
-        }
-        System.out.print(System.lineSeparator() + System.lineSeparator());
-        System.out.print(ANSI_GREEN + "Triangle Numbers: " + ANSI_RESET);
+    public static void search(int[] LucasNum, int len) {
+        int number = 1;
+        while (number != 0) {
+            System.out.print(System.lineSeparator());
+            Scanner nu = new Scanner(System.in);
+            System.out.println("Enter queue of Lucas Number you want to find: (0 to stop)");
+            number = nu.nextInt();
+            for (int k = 0; k < len + 1; k++) {
 
-        // вивід трикутних чисел
-        for (int k = 0; k < len; k++) {
-            System.out.print(TriangleNum[k] + " ");
+                if (k == number) {
+                    System.out.println("You found " + LucasNum[k - 1]);
+                }
+            }
         }
-        System.out.print(System.lineSeparator() + System.lineSeparator());
+    }
 
-        // Числа Люка які = трикутним числам
+    public static void compare(int[] LucasNum, int[] TriangleNum, int len) {
         for (int k = 0; k < len; k++) {
             if (LucasNum[k] == TriangleNum[k]) {
                 System.out.println(ANSI_RED + "Lucas " + LucasNum[k] + " = Tringle " + TriangleNum[k] + ANSI_RESET);
@@ -53,19 +32,50 @@ public class App {
                 System.out.println("Lucas " + LucasNum[k] + " != Tringle " + TriangleNum[k]);
             }
         }
-        int number = 1;
-        System.out.print(System.lineSeparator());
-        while(number != 0) {   
-        System.out.print(System.lineSeparator());
-        Scanner nu = new Scanner(System.in);  
-        System.out.println("Enter queue of Lucas Number you want to find: (0 to stop)");
-        number = nu.nextInt();
-            for (int k = 0; k < len + 1; k++) {
+    }
 
-                if (k == number) {
-                    System.out.println("You found " + LucasNum[k-1]);
-                }
-            }
-        } 
+    public static void main(String[] args) {
+        App obj = new App();
+        // змінні і масиви
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the length of lucas series:");
+        obj.len = in.nextInt();
+        // int[] LucasNum = new int[obj.len];
+        // int[] TriangleNum = new int[obj.len];
+        int j = 0;
+        int count = 2;
+        // масив чисел Люка
+        for (obj.i = 1; obj.i <= obj.len; obj.i++) {
+            obj.c = obj.a + obj.b;
+            obj.LucasNum[j] = obj.c;
+            obj.a = obj.b;
+            obj.b = obj.c;
+            j++;
+        }
+        // вивід чисел Люка
+        System.out.print(System.lineSeparator());
+        System.out.print(ANSI_GREEN + "Lucas Numbers: " + ANSI_RESET);
+        for (int k = 0; k < obj.len; k++) {
+            System.out.print(obj.LucasNum[k] + " ");
+        }
+        // масив трикутних чисел
+        for (int k = 0; k < obj.len; k++) {
+            obj.TriangleNum[k] = ((count * (count + 1)) / 2);
+            count++;
+        }
+        System.out.print(System.lineSeparator() + System.lineSeparator());
+        System.out.print(ANSI_GREEN + "Triangle Numbers: " + ANSI_RESET);
+
+        // вивід трикутних чисел
+        for (int k = 0; k < obj.len; k++) {
+            System.out.print(obj.TriangleNum[k] + " ");
+        }
+        System.out.print(System.lineSeparator() + System.lineSeparator());
+
+        // Числа Люка які = трикутним числам
+        compare(obj.LucasNum, obj.TriangleNum, obj.len);
+        // пошук чисел люка
+        System.out.print(System.lineSeparator());
+        search(obj.LucasNum, obj.len);
     }
 }
